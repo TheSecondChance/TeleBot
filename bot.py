f390a1042d624a9313e66239034f935f69a9dca3
TheSecondChance <mesakmegerem@gmail.com>
2024-03-31 02:02:52 -0700

    Add help command

--- bot.py
+++ bot.py
@@ -36,6 +36,24 @@ def send_game_message(message):
   bot.send_message(chat_id=message.chat.id, text="see you agin",
                    reply_markup=markup)
 
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(
+        telebot.types.InlineKeyboardButton(
+            'Message the developer', url='https://t.me/eyasu_dev'
+        )
+    )
+    bot.send_message(
+        message.chat.id,
+        '1) To receive a list of available command press /ToRegister.\n ' +
+        '2) Click on.\n' +
+        '3) You will receive a message containing information regarding the source, ' +
+        'contact.\n' +
+        '4) Click “Update” to receive the current information regarding the request. ',
+        reply_markup=keyboard
+    )
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text + " not correcte")
